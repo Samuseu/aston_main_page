bd3fa62e21ac0441cd012117710710f0e0b655ff
Vlad <s67.freelance@gmail.com>
2024-01-19 17:18:16 +0300

    Initial commit

---
+++ src/test/java/ru/astondesvs/constants/Constant.java
@@ -0,0 +1,7 @@
+package ru.astondesvs.constants;
+
+public class Constant {
+    public final static String
+            TITLE_TEXT_CENTER_PAGE = "",
+            TITLE_MAIN = ")";
+}
---
+++ src/test/java/ru/astondesvs/pages/MainPage.java
@@ -0,0 +1,4 @@
+package ru.astondesvs.pages;
+
+public class MainPage {
+}
